80c3eaa6afa4040e17b645d749aecc81d1e10d21
Viana <viana@iMac-Pro-de-Viana.local>
2022-01-20 15:57:51 -0300

    Project Created

---
+++ backend/src/main/java/com/jrcg/jrcglearn/JrcglearnApplication.java
@@ -0,0 +1,13 @@
+package com.jrcg.jrcglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrcglearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrcglearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jrcg/jrcglearn/JrcglearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrcg.jrcglearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrcglearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
