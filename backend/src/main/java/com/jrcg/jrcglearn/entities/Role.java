8d57a6228ea1796c34472cc33baed565c766fcda
Viana <viana@iMac-Pro-de-Viana.local>
2022-01-20 18:04:52 -0300

    User, Role

---
+++ backend/src/main/java/com/jrcg/jrcglearn/entities/Role.java
@@ -0,0 +1,27 @@
+package com.jrcg.jrcglearn.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	
+}
