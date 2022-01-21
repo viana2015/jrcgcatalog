c70c41f27dc1b12b0c821903d549883b0e1c071b
Viana <viana@iMac-Pro-de-Viana.local>
2022-01-20 22:21:20 -0300

    Course

---
+++ backend/src/main/java/com/jrcg/jrcglearn/entities/Course.java
@@ -0,0 +1,28 @@
+package com.jrcg.jrcglearn.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String imgUri;
+	private String imgGrayUri;
+	
+}
