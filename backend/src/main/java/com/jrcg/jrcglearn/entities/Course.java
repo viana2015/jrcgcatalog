342e3a671f1b0c00bf981857090b91a4e4c14957
Viana <viana@iMac-Pro-de-Viana.local>
2022-01-22 16:13:31 -0300

    Offer

--- backend/src/main/java/com/jrcg/jrcglearn/entities/Course.java
+++ backend/src/main/java/com/jrcg/jrcglearn/entities/Course.java
@@ -1,28 +1,41 @@
 package com.jrcg.jrcglearn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.Setter;
 
 @Data
-@AllArgsConstructor
 @Entity
 @Table(name = "tb_course")
 public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NonNull
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NonNull
 	private String name;
+	@NonNull
 	private String imgUri;
+	@NonNull
 	private String imgGrayUri;
+	
+	
+	@Setter(value = AccessLevel.NONE)
+	@OneToMany(mappedBy = "course")
+	private List<Offer>offers = new ArrayList<>();
 	
 }
---
+++ backend/src/main/java/com/jrcg/jrcglearn/entities/Offer.java
@@ -0,0 +1,37 @@
+package com.jrcg.jrcglearn.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String edition;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant startMoment;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant endMoment;
+	
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+}
